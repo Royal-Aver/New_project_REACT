5ea916ff481f0180e7ca23cfa18daae16727c443
Royal-Aver <royal_aver@rambler.ru>
2022-07-12 06:06:22 +0300

    version 1.0 added node-sass

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import './Content.scss'
 
 const Content = () => {
   return (
     <div className="content">
       <div className="content__container container">
+
       </div>
     </div>
   )
